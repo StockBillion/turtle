20f9a978549a50e39cf400da2e2d0021e75157da
jiyu <jyzhu@shuangjisha.com>
2019-05-24 20:17:47 +0800

    add strong50.py

---
+++ strong50.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#-*- coding: utf8 -*-
+import numpy, pandas, math, datetime as dt, time
+from turtle import trade, index, data, utils
+from turtle.data import StockDataSource
+
+
